9b644b4987db51686414a246a3c18668f438c7ff
Harshiv19 <75972903+Harshiv19@users.noreply.github.com>
2021-10-09 22:13:18 +1300

    Add files via upload

---
+++ Project 28/Project-C28-template-main/js/launch.js
@@ -0,0 +1,34 @@
+class launch{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.003,
+            length: 10
+        }
+        this.bodyA = bodyA
+        this.pointB = pointB
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+
+   
+
+    display(){
+        if(this.launcher.bodyA){
+            var pointA = this.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(3);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+   
+    }
+
+     attach(body){
+        this.launcher.bodyA = body;
+      }
+
+      fly(){
+this.launcher.bodyA = null
+      }
+}
